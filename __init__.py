6e7cd5ccd5bc7e04f2a55334a21e1f37c0816cea
Garrett Goon <garrett361@gmail.com>
2024-12-12 01:51:44 +0000

    made into a module

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from dtest._dtest import (
+    DTest,
+    get_rank,
+    get_world_size,
+    get_device_type,
+    get_device,
+    get_backend,
+    get_num_gpus,
+    print_rank,
+    print_rank0_only,
+)
